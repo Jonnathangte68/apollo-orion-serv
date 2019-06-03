c789be9ade35c31570356f5ea73c99684d09b4ec
Jonnathan Guarate <jguarate@arkusnexus.com>
2019-06-03 12:50:03 -0700

    Other changes server add the link

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ const authors = [
   { id: 3, firstName: 'Chimamanda', lastName: 'Adichie' },
 ];
 
+const users = [
+  { id: 1, firstName: 'Wole', lastName: 'Soyinka' }
+];
+
+const comments = [
+  { id: 1, description: 'Wole', userId: 1 }
+];
+
+const links = [
+  { id: 1, title: 'Apple Sign In', points: 8, userId: 1, website: 'techcrunch.com', created: Date },
+  { id: 2, title: '	WWDC 2019 Keynote Livestream ', points: 25, userId: 1, website: 'foundationinc.co', created: Date },
+];
+
 const typeDefs = gql`
   type Author {
     id: Int!
@@ -44,6 +57,7 @@ let bookId = 5;
 const resolvers = {
   Query: {
     books: () => books,
+    links: () => links,
     book: (_, { id }) => books.find(book => book.id === id),
     author: (_, { id }) => authors.find(author => author.id === id),
   },
@@ -60,6 +74,21 @@ const resolvers = {
 
       books.push(newBook);
       return newBook;
+    },
+    addLink: (_, { title, points, userId, website, created }) => {
+      linkId++;
+
+      const newLink = {
+        id: linkId,
+        title, 
+        points, 
+        userId, 
+        website, 
+        created
+      };
+
+      links.push(newLink);
+      return newLink;
     },
   },
   Author: {
