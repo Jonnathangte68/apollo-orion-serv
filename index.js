9e83a1357e7296a254703fa86853d65e5408bba2
Jonnathan Guarate <jguarate@arkusnexus.com>
2019-06-03 13:23:28 -0700

    Asgasdg

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const { ApolloServer, gql } = require('apollo-server-micro');
 
 const books = [
-  { id: 1, title: 'The Trials of Brother Jero', rating: 8, authorId: 1 },
-  { id: 2, title: 'Half of a Yellow Sun', rating: 9, authorId: 3 },
-  { id: 3, title: 'Americanah', rating: 9, authorId: 3 },
-  { id: 4, title: 'King Baabu', rating: 9, authorId: 1 },
-  { id: 5, title: 'Children of Blood and Bone', rating: 8, authorId: 2 },
-];
-
-const authors = [
-  { id: 1, firstName: 'Wole', lastName: 'Soyinka' },
-  { id: 2, firstName: 'Tomi', lastName: 'Adeyemi' },
-  { id: 3, firstName: 'Chimamanda', lastName: 'Adichie' },
+  { id: 1, title: 'The Trials of Brother Jero', rating: 8 },
+  { id: 2, title: 'Half of a Yellow Sun', rating: 9 },
+  { id: 3, title: 'Americanah', rating: 9 },
+  { id: 4, title: 'King Baabu', rating: 9 },
+  { id: 5, title: 'Children of Blood and Bone', rating: 8 },
 ];
 
 const links = [
@@ -20,17 +14,10 @@ const links = [
 ];
 
 const typeDefs = gql`
-  type Author {
-    id: Int!
-    firstName: String!
-    lastName: String!
-    books: [Book]! # the list of books by this author
-  }
   type Book {
     id: Int!
     title: String!
     rating: Int!
-    author: Author!
   }
   type Link {
     id: Int!
@@ -42,13 +29,13 @@ const typeDefs = gql`
   # the schema allows the following query
   type Query {
     books: [Book!]!
+    links: [Link!]!
     book(id: Int!): Book!
-    author(id: Int!): Author!
     link(id: Int!): Link!
   }
   # this schema allows the following mutation
   type Mutation {
-    addBook(title: String!, rating: Int!, authorId: Int!): Book!
+    addBook(title: String!, rating: Int!): Book!
   }
 `;
 
@@ -59,43 +46,21 @@ const resolvers = {
     books: () => books,
     links: () => links,
     book: (_, { id }) => books.find(book => book.id === id),
-    author: (_, { id }) => authors.find(author => author.id === id),
     link: (_, { id }) => links.find(link => link.id === id),
   },
   Mutation: {
-    addBook: (_, { title, rating, authorId }) => {
+    addBook: (_, { title, rating }) => {
       bookId++;
 
       const newBook = {
         id: bookId,
         title,
         rating,
-        authorId,
       };
 
       books.push(newBook);
       return newBook;
     },
-    addLink: (_, { title, points, userId, website, created }) => {
-      linkId++;
-
-      const newLink = {
-        id: linkId,
-        title, 
-        points, 
-        website, 
-        created
-      };
-
-      links.push(newLink);
-      return newLink;
-    },
-  },
-  Author: {
-    books: author => books.filter(book => book.authorId === author.id),
-  },
-  Book: {
-    author: book => authors.find(author => author.id === book.authorId),
   },
 };
 
